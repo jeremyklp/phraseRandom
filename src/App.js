34e8d909ad5dcdc0d908ad1994b436df8b1e092a
jeremy <jeremyvallejo2016@gmail.com>
2022-05-02 09:58:37 -0500

    esta monda

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Phrase from './components/Phrase';
+
+function App() {
+
+  
+
+  return (
+
+
+    <div
+    className="App">
+     <Phrase/>
+     
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Phrase.js
@@ -0,0 +1,42 @@
+import quotes from "../quotes.json"
+import {useState} from 'react'
+
+const Phrase = () => {
+    const randomIndex = Math.floor(Math.random () * quotes.length)
+    const [random, setramdom] = useState(randomIndex);
+    let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    const changephrase = ()=>{ 
+    const randomIndex = Math.floor(Math.random () * quotes.length)
+    setramdom(randomIndex)
+    }
+    
+    return (
+        <div className="fff"
+        style={{
+            backgroundColor:`${randomColor}`
+        }}>
+            <div className='centrado'>
+                   
+                    <div className="text">
+                        <h5 style={{color:`${randomColor}`}}>
+                        <i class="fa-solid fa-quote-left"></i>{quotes[random].quote}
+                        </h5>
+                       <div className="right">
+                         <h3 style={{ color:`${randomColor}`}}>
+                            {quotes[random].author}
+                            </h3>
+                           </div> 
+                    </div>
+                    <div 
+                        className="but">
+                        <a 
+                        style={{backgroundColor:`${randomColor}`, color:"black"}} href="" onClick={changephrase}>
+                        <i class="fa-solid fa-arrow-right"></i>
+                        </a>
+                    </div>
+            </div>
+        </div>
+    );
+};
+
+export default Phrase;
